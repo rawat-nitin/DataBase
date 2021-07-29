9e8e65afc691253107e8daa7f5d79f8f8495841a
rawat-nitin <rsnitin.inv@gmail.com>
2021-07-29 13:53:59 +0900

    repository

--- src/main/kotlin/com/example/database/repository/DataBaseApplication.kt
+++ src/main/kotlin/com/example/database/repository/DataBaseApplication.kt
@@ -1,4 +1,4 @@
-package com.example.database
+package com.example.database.repository
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
